98fd0090b1606e79cfb9ac6ce2fe024c85c75abc
SaMuELToLoKo <37693787+SaMuELToLoKo@users.noreply.github.com>
2018-03-25 19:43:29 +0100

    Add files via upload

---
+++ getWaterState.ino
@@ -0,0 +1,24 @@
+// Function to get Current State.
+
+byte getWaterState(byte value) {
+
+  byte water = 0;
+  byte check = value;
+  check &= B10000000; // First of all we delete all the target states
+  
+  switch (check) {
+    case B00000000:
+      water = 0; // "State Off".
+      break;
+
+    case B10000000:
+      water = 1; // "State Heat".
+      break;
+
+    default:
+      Serial.println("Error getting Current State data");
+      break;
+  }
+return water;
+}
+
